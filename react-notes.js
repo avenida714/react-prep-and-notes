bd6226884f345da53238c10bda70a9b9fe972392
Alec Venida <alecvenida@gmail.com>
2022-07-07 12:48:35 -0700

    don't forget to import you Profile component in src/indes.js

--- react-notes.js
+++ react-notes.js
@@ -129,3 +129,18 @@ import {useParams} from 'react-router-dom';
 function Example() {
   const params = useParams();
 }
+
+
+//// ./src/components/Profile.js
+
+import React from "react";
+import {useParams} from 'react-router-dom';
+
+const Profile = () => {
+  const params = useParams();
+  const {userId} = params;
+
+  return <h1> Hello from User Profile {userId}!</h1>
+}
+
+export default Profile;
