fa1c4645547febcb8736175850a33c60276d1888
Alec Venida <alecvenida@gmail.com>
2022-07-07 11:43:57 -0700

    browser router is the primary component of the router that wraps your route

--- react-notes.js
+++ react-notes.js
@@ -3,3 +3,27 @@
 //npx create-react-app my-app --template @appacademy/react-v17 --use-npm
 
 // cd my-app && npm install --save react-router-dom@^5.1.2
+
+
+//npm start
+
+
+//./src/index.js
+import {BrowserRouter} from 'react-router-dom';
+
+
+
+//./src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import App from './App';
+
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+};
