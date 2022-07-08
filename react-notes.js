fe9a890e4509193d2acb9b1270a419b63ec66dbd
Alec Venida <alecvenida@gmail.com>
2022-07-08 13:24:49 -0700

    root component with NavLinks

--- react-notes.js
+++ react-notes.js
@@ -234,3 +234,45 @@ import {BrowserRouter, Route, Navlink} from 'react-router-dom';
 // <NavLink to="/" exact={true} activeStyle={{ fontWeight: "bold" }}>App</NavLink>
 
 // <NavLink to="/" exact onClick={handleClick}>App with click handler</NavLink>
+
+
+//SWITCHING BETWEEN ROUTES
+
+const Root3029483209409 = () => {
+  const handleClick = () => {
+    console.log("Thanks for clicking")
+  };
+
+  return (
+    <BrowserRouter>
+      <div>
+        <NavLink to="/" exact={true} activeStyle={{ fontWeight: "bold"}}>App</NavLink>
+        <NavLink activeClassName="red" to="/users">Users</NavLink>
+        <NavLink activeClassName="blue" to="/hello">Hello</NavLink>
+        <NavLink activeClassName="black" to="/users/1">Bobo's Profile</NavLink>
+        {/* NavLink with onClick prop */}
+
+        <NavLink to="/" exact onClick={handleClick}>App with click handler</NavLink>
+
+        <Switch>
+          <Route exact path="/">
+            <App/>
+          </Route>
+
+          <Route path="/users">
+            <Users />
+          </Route>
+
+          <Route path="/users/:userId">
+            <Profile />
+          </Route>
+
+          <Route>
+            <h1>404: Page not found</h1>
+          </Route>
+        </Switch>
+
+      </div>
+    </BrowserRouter>
+  );
+};
