7827c9483f860af3a95791b076496234997e6a18
Alec Venida <alecvenida@gmail.com>
2022-07-10 19:26:59 -0700

    useDispatch

--- front-practice-for-week-15-react-thunk-pokedex-long-practice-main/src/components/PokemonBrowser.js
+++ front-practice-for-week-15-react-thunk-pokedex-long-practice-main/src/components/PokemonBrowser.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, Route, useParams } from 'react-router-dom';
 
 import PokemonDetail from './PokemonDetail';
