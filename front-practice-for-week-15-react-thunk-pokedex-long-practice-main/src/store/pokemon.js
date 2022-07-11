41115b8465650b99c89b2dc71677cbaa440f10a1
Alec Venida <alecvenida@gmail.com>
2022-07-10 20:58:10 -0700

    start part 2 getOnePokemon

--- front-practice-for-week-15-react-thunk-pokedex-long-practice-main/src/store/pokemon.js
+++ front-practice-for-week-15-react-thunk-pokedex-long-practice-main/src/store/pokemon.js
@@ -37,6 +37,16 @@ export const getPokemonTypes = () => async dispatch => {
   }
 };
 
+export const getOnePokemon = (pokemonId) => async (dispatch) => {
+  const res = await fetch(`/api/pokemon/${pokemonId}`);
+
+  if (res.ok) {
+    const pokemon = await res.json();
+    dispatch(addOnePokemon(pokemonId));
+    return
+  }
+}
+
 const initialState = {
   list: [],
   types: []
@@ -50,7 +60,7 @@ const sortList = (list) => {
 
 const pokemonReducer = (state = initialState, action) => {
   switch (action.type) {
-    case LOAD: 
+    case LOAD:
       const allPokemon = {};
       action.list.forEach(pokemon => {
         allPokemon[pokemon.id] = pokemon;
@@ -60,12 +70,12 @@ const pokemonReducer = (state = initialState, action) => {
         ...state,
         list: sortList(action.list)
       };
-    case LOAD_TYPES: 
+    case LOAD_TYPES:
       return {
         ...state,
         types: action.types
       };
-    case ADD_ONE: 
+    case ADD_ONE:
       if (!state[action.pokemon.id]) {
         const newState = {
           ...state,
@@ -83,7 +93,7 @@ const pokemonReducer = (state = initialState, action) => {
           ...action.pokemon
         }
       };
-    case LOAD_ITEMS: 
+    case LOAD_ITEMS:
       return {
         ...state,
         [action.pokemonId]: {
